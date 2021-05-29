0faf137b59644be6672c6fc83ceef3b491ce9c7d
Stien Vanderhallen <stien.vanderhallen@hotmail.com>
2021-05-29 13:55:14 +0200

    Talking

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ WT_BOT_TOKEN = os.environ['WT_BOT_TOKEN']
 #WT_ROOM_ID = os.environ['WT_ROOM_ID']
 
 # uncomment next line if you are implementing a controller bot
-#WT_BOT_EMAIL = os.environ['WT_BOT_EMAIL']
+WT_BOT_EMAIL = os.environ['WT_BOT_EMAIL']
 
 # start Flask and WT connection
 app = Flask(__name__)
@@ -46,12 +46,10 @@ def alert_received():
 
 
     # uncomment if you are implementing a controller bot
-    '''
     WT_ROOM_ID = raw_json['data']['roomId']
     personEmail_json = raw_json['data']['personEmail']
     if personEmail_json != WT_BOT_EMAIL:
         api.messages.create(roomId=WT_ROOM_ID, markdown=message)
-    '''
 
     return jsonify({'success': True})
 
